8cd644941a48cc119bb570b85feb9bb6d0a29cfa
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2022-12-21 15:52:16 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -22,17 +22,16 @@ var clouds; //variavel para nuvens (grupo) 07/12
 var gameOver, restart; //variavel para Fim de Jogo e Reiniciar 12/12
 
 var gameOverImg, restartImg; //variavel para imagem de Game Over e Reiniciar 12/12
+
+var jumpSound , checkPointSound, dieSound; //variaveis para os Sons do jogo 14/12
+
 //---------------------------------------
 function preload(){
   //Preload vai carregar arquivos de imagem e som
   trex_running = loadAnimation ("trex1.png" , "trex3.png" , "trex4.png"); //trex correndo
-
   trex_collided = loadImage ("trex_collided.png"); //trex surpreso pela colisão 28/11
-
   groundImage = loadImage("ground2.png"); //imagem do chão 28/11
-
   cloudImage = loadImage("cloud.png"); //imagem da nuvem 30/11
-
   //imagens para os obstáculos 05/12
   obstacle1 = loadImage("obstacle1.png");
   obstacle2 = loadImage("obstacle2.png");
@@ -40,10 +39,13 @@ function preload(){
   obstacle4 = loadImage("obstacle4.png");
   obstacle5 = loadImage("obstacle5.png");
   obstacle6 = loadImage("obstacle6.png");
-
   //imagens para reiniciar e game over 12/12
   restartImg = loadImage("restart.png");
   gameOverImg = loadImage("gameOver.png");
+  // carregar som 14/12
+  jumpSound = loadSound("jump.mp3");
+  dieSound = loadSound("die.mp3");
+  checkPointSound = loadSound("checkpoint.mp3");
 }
 //---------------------------------------
 function setup(){
@@ -103,11 +105,16 @@ function draw(){
   background("white");
   //----------ESTADO DE JOGO JOGAR(PLAY)-----------
   if (gamestate === PLAY){ //estado de JOGAR
+      //velocidade do chão aumenta conforme a pontuação 19/12
+    ground.velocityX = -(4 + 3 * score/100);
       //calcula a pontuação dividindo o total de frames gerados por 60 07/12
-    score = score + Math.round(frameCount/60);
+    //score = score + Math.round(frameCount/60);
+      //calculando a pontuação com maior precisão de frames gerados 21/12
+    score = score + Math.round(getFrameRate/60);
       //pular quando a tecla espaço for pressionada 28/11 E 07/12
     if(keyDown("space") && trex.y >= 100) {
       trex.velocityY = -10;
+      jumpSound.play(); //toca o som sempre que pula 14/12
      }
       //trex voltar ao chão depois do pulo 28/11 E 07/12
     trex.velocityY = trex.velocityY + 0.8
@@ -121,15 +128,18 @@ function draw(){
     spawnClouds(); 
       //chama a geração de obstaculos 05/12 E 07/12
     spawnObstacles(); 
-    //
       //se os obstaculos tocarem o trex, o jogo acaba 12/12
     if(obstacles.isTouching(trex))
         { 
           gamestate = END; // mudar o estado de jogo para Final
+          dieSound.play(); //toca o som quando encosta no obstaculo 14/12
         }
-
     gameOver.visible = false; //não tem visibilidade da imagem de GameOver 12/12
     restart.visible = false; //não tem visibilidade da imagem de Restart 12/12
+    //se a pontuação for maior que 0 e for divisivel por 100 (100, 200, 300...) 14/12
+    if(score>0 && score%100 === 0){
+      checkPointSound.play(); //toca o som a cada 100 pts
+    }
   }
   //----------ESTADO DE JOGO FINAL(END)-----------
   else if (gamestate === END) { //estado de FINAL 07/12
@@ -144,12 +154,17 @@ function draw(){
       restart.visible = true; //visibilidade da imagem de Restart 12/12
 
       //definir tempo de vida aos objetos do jogo para que nunca sejam destruídos 12/12
-       obstacles.setLifetimeEach(-1);
-        clouds.setLifetimeEach(-1);
+       obstacles.setlifetimeEach(-1);
+       clouds.setlifetimeEach(-1);
 
      //definir velocidade aos objetos do jogo para que nunca de movam 12/12
        obstacles.setVelocityXEach(0);
        clouds.setVelocityXEach(0);
+
+    //se o mouse clicar no botão de reiniciar, vai ser chamada a função reset 21/12
+       if (mousePressedOver(restart)) {
+        reset();
+       }
   }
   
   drawSprites();
@@ -181,7 +196,7 @@ function spawnClouds(){
 function spawnObstacles(){
   if (frameCount % 60 === 0){
     var obstacle = createSprite(400,165,10,40);
-    obstacle.velocityX = -6;
+    obstacle.velocityX = -(6 + score/100); //a velocidade aumenta conforme cada 100 PONTOS são contados 19/12
      //gerar obstáculos aleatórios
      var rand = Math.round(random(1,6));
      switch(rand) {
@@ -205,4 +220,20 @@ function spawnObstacles(){
      //adiciona os obstáculos ao grupo de obstáculos 12/12
      obstacles.add(obstacle);
   }
+ }
+
+
+ function reset(){ //vai reiniciar o jogo 21/12
+  //muda do END pro PLAY
+    gamestate = PLAY;
+  //sprites de gameover e restart invisiveis 
+    gameOver.visible = false;
+    restart.visible = false;
+  //destruímos os obstaculos e as nuvens geradas do jogo anterior
+    obstacles.destroyEach();
+    clouds.destroyEach();
+  //pontuação volta a ser zero
+    score = 0;
+  //trex volta a ter a animação de corrida
+    trex.changeAnimation("running",trex_running);
  }
